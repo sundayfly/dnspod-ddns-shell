b19a49133ba7857afdc62a3d622eb3190086f821
sunday <shaopenghk@qq.com>
2021-08-05 18:04:12 +0800

    github

--- dnspod-ddns.sh
+++ dnspod-ddns.sh
@@ -16,8 +16,8 @@ record_value=$(echo ${record_info} | grep -Po "(?<=\"value\":\")([0-9\.]*)")
 record_line_id=$(echo ${record_info} | grep -Po "(?<=\"line_id\":\")([0-9\.]*)")
 echo "${SUB_DOMAIN}:${DOMAIN} record_id: ${record_id}, record_line_id: ${record_line_id}, record_value: ${record_value}"
 
-local_ip=$(curl -s https://api.ipify.org)
-#local_ip=$(curl -s ns1.dnspod.net:6666)
+#local_ip=$(curl -s https://api.ipify.org)
+local_ip=$(curl -s http://ip.qaros.com/)
 echo "local ip: ${local_ip}"
 
 if [[ $record_value != $local_ip ]];then
@@ -30,6 +30,5 @@ fi
 
 #https://gist.github.com/ankanch/8c8ec5aaf374039504946e7e2b2cdf7f
 #https://www.ipify.org
-#http://myexternalip.com
 #http://api.ident.me
-#http://whatismyipaddress.com/api
+#http://ip.3322.net
