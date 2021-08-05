5dbcfa514537a5935dfe0841de212fa24f365fa4
sunday <shaopenghk@qq.com>
2021-08-05 17:53:03 +0800

    add ip information

--- dnspod-ddns.sh
+++ dnspod-ddns.sh
@@ -16,7 +16,6 @@ record_value=$(echo ${record_info} | grep -Po "(?<=\"value\":\")([0-9\.]*)")
 record_line_id=$(echo ${record_info} | grep -Po "(?<=\"line_id\":\")([0-9\.]*)")
 echo "${SUB_DOMAIN}:${DOMAIN} record_id: ${record_id}, record_line_id: ${record_line_id}, record_value: ${record_value}"
 
-
 local_ip=$(curl -s https://api.ipify.org)
 #local_ip=$(curl -s ns1.dnspod.net:6666)
 echo "local ip: ${local_ip}"
@@ -28,3 +27,9 @@ if [[ $record_value != $local_ip ]];then
 else
   echo "not changed, ignore"
 fi
+
+#https://gist.github.com/ankanch/8c8ec5aaf374039504946e7e2b2cdf7f
+#https://www.ipify.org
+#http://myexternalip.com
+#http://api.ident.me
+#http://whatismyipaddress.com/api
